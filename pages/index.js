4193e7e3aec4909dabb573dad4f912af183ba52a
shobhit707 <50417428+shobhit707@users.noreply.github.com>
2024-04-25 18:04:31 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -152,7 +152,7 @@ export default function Home()
       {
         confirmed ? 
         <div className="flex flex-col justify-around items-center text-center w-full h-[58em] bg-pink-200 p-5">
-          <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>Prom Confirmation Ticket / Review</h1>
+          <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>Cuddle Session Confirmation Ticket / Review</h1>
           <div className="flex flex-col items-center gap-5 text-center w-3/4 h-5/6 bg-white p-5 rounded-3xl shadow-xl">
             <img className="h-24" src="https://cdn.pixabay.com/photo/2018/02/19/20/28/abstract-3166168__480.png" alt=""/>
             <h1 className='w-2/3 text-black font-bold text-4xl underline'>
